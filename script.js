ea673b98abb3cca6138e6438863013c7a60cafb0
Paul Thurmond <paulthurmond@gmail.com>
2023-01-08 07:52:22 -0600

    Added event listeners

--- script.js
+++ script.js
@@ -37,6 +37,55 @@ const gameState = (() => {
   return { board, turn };
 })();
 
-gameState.board[5] = 1;
-console.log(gameState.board);
-console.log(gameState.turn);
+const initialize = () => {
+  for (let i = 0; i <= 8; i++) {
+    let block = document.getElementById(`block_${i}`);
+    block.addEventListener(
+      "click",
+      function () {
+        makeMove(i, block, gameState);
+      },
+      false
+    );
+  }
+};
+
+// const b0 = document.getElementById("block_0");
+// b0.addEventListener(
+//   "click",
+//   function () {
+//     makeMove(b0, gameState);
+//   },
+//   false
+// );
+
+const makeMove = function (i, block, gameState) {
+  if (gameState.board[i] === 0) {
+    if (gameState.turn === 1) {
+      gameState.board[i] = 1;
+      block.textContent = "X";
+      checkForGameOver(gameState);
+      gameState.turn = 2;
+    } else {
+      gameState.board[i] = 2;
+      block.textContent = "O";
+      checkForGameOver(gameState);
+      gameState.turn = 1;
+    }
+  }
+};
+
+const checkForGameOver = function (gameState) {
+  const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+};
+
+initialize();
